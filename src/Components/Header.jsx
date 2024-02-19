2ae2e8d7fe874132989af06beb51864d4d1a350b
Yousuf Mohammadi <theehsansarshar@Yousufs-MacBook-Air.local>
2024-02-19 13:13:31 +0430

    header styled

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div class="w-full h-20 bg-white-500 flex flex-row items-center bg-red-400">
+      <a
+        href="/home"
+        class="font-semibold text-white hover:text-sky-400  text-center w-full"
+      >
+        useState
+      </a>
+      <a
+        href="/home"
+        class="font-semibold text-white hover:text-sky-400 text-center w-full"
+      >
+        useEffect
+      </a>
+      <a
+        href="/home"
+        class="font-semibold text-white hover:text-sky-400 text-center w-full"
+      >
+        useContext
+      </a>
+      <a
+        href="/home"
+        class="font-semibold text-white hover:text-sky-400 text-center w-full"
+      >
+        useRef
+      </a>
+      <a
+        href="/home"
+        class="font-semibold text-white hover:text-sky-400 text-center w-full"
+      >
+        useReducer
+      </a>
+      <a
+        href="/home"
+        class="font-semibold text-white hover:text-sky-400 text-center w-full"
+      >
+        useLayoutEffect
+      </a>
+      <a
+        href="/home"
+        class="font-semibold text-white hover:text-sky-400 text-center w-full"
+      >
+        useCallBack
+      </a>
+      <a
+        href="/home"
+        class="font-semibold text-white hover:text-sky-400 text-center w-full"
+      >
+        customHook
+      </a>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
