54d00f31179dad021bb36366edae1f54e41fae43
Yousuf Mohammadi <theehsansarshar@Yousufs-MacBook-Air.local>
2024-02-19 12:29:48 +0430

    tailwindCSS installed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
+import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 function Home() {
   return (
-    <div className='home-container'>
-    <Header />
-    <div>container</div>
-    <Footer />
+    <div className="home-container">
+      <Header />
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
